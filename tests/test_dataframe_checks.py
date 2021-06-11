8d71d1a85ecc313887e3e3876e838d7cf397ceb9
amit2020cs <amit2020cs@gmail.com>
2021-06-12 01:08:49 +0530

    Stock Data

---
+++ tests/test_dataframe_checks.py
@@ -0,0 +1,27 @@
+
+from pandas.testing import assert_frame_equal
+import pytest
+
+import sys 
+import os
+
+sys.path.append(os.path.abspath(''))
+
+
+from dataframe_checks import (
+    _check_contains_na,
+    _check_input_matches_training_df,
+    _is_dataframe,
+)
+
+
+def test_stock_df_is_dataframe(stock_df):
+    assert_frame_equal(_is_dataframe(stock_df), stock_df)
+    with pytest.raises(TypeError):
+        assert _is_dataframe([1, 2, 4])
+
+
+def test_check_input_matches_training_df(stock_df):
+    with pytest.raises(ValueError):
+        assert _check_input_matches_training_df(stock_df, 4)
+
---
+++ tests/test_parameter_checks.py
@@ -0,0 +1,21 @@
+import pytest
+import sys 
+import os
+
+sys.path.append(os.path.abspath(''))
+
+from parameter_checks import _define_numerical_dict
+
+
+def test_not_numerical_dict():
+    input_dict = {"a": 1, "b": "c"}
+
+    with pytest.raises(ValueError):
+        assert _define_numerical_dict(input_dict)
+
+
+def test_input_type():
+    input_dict = [1, 2, 3]
+
+    with pytest.raises(TypeError):
+        assert _define_numerical_dict(input_dict)
